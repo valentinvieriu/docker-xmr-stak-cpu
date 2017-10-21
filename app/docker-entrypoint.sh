7424231368fa16c8b2dcd2bb426133244c0a5235
Valentin Vieriu <valentin.vieriu@gmail.com>
2017-10-21 11:23:22 +0200

    latest

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -8,8 +8,11 @@ fi
 export AUTO_CONFIGURATION=$(/app/xmr-stak-cpu | grep "low_power_mode")
 
 envtpl /app/xmr-stak-cpu.conf.tpl -o /app/xmr-stak-cpu.conf --allow-missing --keep-template
+if [ -z "${SELF}" ]; then
 #this will ping itself
-nohup watch -n 60 curl -G -o /dev/null https://$SELF.herokuapp.com &>/dev/null &
+nohup watch -n 60 curl -G -o /dev/null https://${SELF}.now.sh &>/dev/null &
+fi
+
 
 if [ "$1" = 'xmr-stak-cpu' ]; then
     exec /app/xmr-stak-cpu /app/xmr-stak-cpu.conf
